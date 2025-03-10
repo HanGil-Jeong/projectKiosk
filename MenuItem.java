7fba412cb96335506848499a3709c78a705d76ae
HanGil <jhg9240@gmail.com>
2025-03-10 19:22:08 +0900

    lv2 Kiosk upload

---
+++ MenuItem.java
@@ -0,0 +1,20 @@
+public class MenuItem{
+
+    // 속성
+    String foodName;
+    String foodItro;
+    double price;
+
+    //생성자
+    public MenuItem(String foodName, double price, String foodItro){
+        this.foodName = foodName;
+        this.foodItro = foodItro;
+        this.price = price;
+    }
+
+    //기능
+
+
+
+
+}
