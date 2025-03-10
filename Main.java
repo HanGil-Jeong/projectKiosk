d85643e215025693dcde140c7c8f044dafab44b0
HanGil <jhg9240@gmail.com>
2025-03-10 21:04:12 +0900

    CommãDocs:

--- Main.java
+++ Main.java
@@ -14,13 +14,6 @@ public class Main {
         menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger   ", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-
-        MenuItem shackBurger = new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem smokeShack = new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem cheeseburger = new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem hamburger = new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거");
-
-
         String choice = "";
 
         while (!choice.equals("0")) {
@@ -33,20 +26,22 @@ public class Main {
             System.out.println("0. 종료      | 종료");
 
             choice = scanner.next();
-            if (choice.equals("1")) {
-                System.out.println("선택한 메뉴: 1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-                System.out.println("");
-            } else if (choice.equals("2")) {
-                System.out.println("선택한 메뉴: 2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                System.out.println("");
-            } else if (choice.equals("3")) {
-                System.out.println("선택한 메뉴: 3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                System.out.println("");
-            } else if (choice.equals("4")) {
-                System.out.println("선택한 메뉴: 4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                System.out.println("");
-            } else if (choice.equals("0")) {
-                System.out.println("프로그램을 종료합니다.");
+            switch (choice) {
+                case "1":
+                    System.out.println("선택한 메뉴: 1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                    System.out.println("");
+                case "2":
+                    System.out.println("선택한 메뉴: 2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                    System.out.println("");
+                case "3":
+                    System.out.println("선택한 메뉴: 3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                    System.out.println("");
+                case "4":
+                    System.out.println("선택한 메뉴: 4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                    System.out.println("");
+                case "0":
+                    System.out.println("프로그램을 종료합니다.");
+
             }
         }
     }
