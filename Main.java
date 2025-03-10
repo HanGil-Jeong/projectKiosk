17ccfb21ee5ea256a9c48a3cb633e03c505f29b0
HanGil <jhg9240@gmail.com>
2025-03-10 20:41:01 +0900

    Comment: 주석추가

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // Scanner 선언
         Scanner scanner = new Scanner(System.in);
 
         String choice = "";
 
+        // while문을 활용하여 입력값이 0이 될 때까지 반복실행
         while (!choice.equals("0")) {
             System.out.println("[ SHAKESHACK MENU]");
             System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
@@ -14,9 +16,10 @@ public class Main {
             System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
             System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             System.out.println("0. 종료      | 종료");
+            // Scanner 활용하여 입력값 받기
             choice = scanner.next();
             if (choice.equals("0")) {
-                System.out.println("프로그램을 종료합니다.");
+                System.out.println("프로그램을 종료합니다."); // 프로그램 종료 전 메세지
             }
         }
     }
