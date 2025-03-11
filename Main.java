44af51fbe802c2a6e6b6b6bf0f7194dbe6e0509a
HanGil <jhg9240@gmail.com>
2025-03-11 15:59:59 +0900

    Comment 주석 추가

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        // 스캐너 선언
         Scanner scanner = new Scanner(System.in);
 
         String choiceNumber;
-
+        // do-while문 활용 Kiosk 반복 실행
         do {
+            // Kiosk클래스 menu 인스턴스화
             Kiosk menu = new Kiosk();
             choiceNumber = scanner.next();
             Kiosk menuChoice = new Kiosk(choiceNumber);
--- MenuItem.java
+++ MenuItem.java
@@ -1,9 +1,9 @@
 public class MenuItem {
-
+    // MenuItem 속성 생성
     String foodName;
     double price;
     String foodIntro;
-
+    // MenuItem 생산자 생성
     MenuItem(String foodName, double price, String foodIntro){
         this.foodName = foodName;
         this.price = price;
