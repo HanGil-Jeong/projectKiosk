de9b6f0ec0e6d8aca5fadeddc575c05bfffd4ace
HanGil <jhg9240@gmail.com>
2025-03-14 12:00:23 +0900

    오íã카ílv4 업로드

--- MenuItem.java
+++ MenuItem.java
@@ -1,12 +1,22 @@
 public class MenuItem {
+
     // MenuItem 속성 생성
     String foodName;
     double price;
     String foodIntro;
+
     // MenuItem 생산자 생성
     MenuItem(String foodName, double price, String foodIntro){
         this.foodName = foodName;
         this.price = price;
         this.foodIntro = foodIntro;
+
     }
+
+    MenuItem(String foodName){
+        this.foodName = foodName;
+    }
+
+
+
 }
